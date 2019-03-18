fdd28b1dfdedd1aa57d4d735fad8fa6795c10554
Lim Cheng Yang <e0203248@u.nus.edu>
2019-03-19 00:35:25 +0800

    added files to ejb source path

---
+++ ZhcqRetailSystem/ZhcqRetailSystem-ejb/src/java/ejb/singleton/InitSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.singleton;
+
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author chengyang
+ */
+@Singleton
+@LocalBean
+public class InitSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}

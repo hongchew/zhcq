abc3fab5d9a14d41e7c8d816e9fae7438dc6c0fc
ZhiMing96 <e0200805@u.nus.edu>
2019-03-26 00:52:10 +0800

    Created Category and Product tag controller

---
+++ ZhcqRetailSystem/ZhcqRetailSystem-ejb/src/java/ejb/stateless/CategoryController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.stateless;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author zhimingkoh
+ */
+@Stateless
+public class CategoryController implements CategoryControllerLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}

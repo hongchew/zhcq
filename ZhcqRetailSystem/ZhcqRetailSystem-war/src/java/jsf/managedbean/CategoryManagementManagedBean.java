a8bc17c8244f516041901e88e5e5e82753f6ac4a
ZhiMing96 <e0200805@u.nus.edu>
2019-03-29 15:56:29 +0800

    OutfitManagement Managed Bean

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/jsf/managedbean/CategoryManagementManagedBean.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/jsf/managedbean/CategoryManagementManagedBean.java
@@ -20,6 +20,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
 import util.exception.CategoryNotFoundException;
 import util.exception.CreateNewCategoryException;
 import util.exception.CreateNewProductException;
@@ -29,8 +30,8 @@ import util.exception.UpdateCategoryException;
 
 
 @Named(value = "categoryManagementManagedBean")
-@RequestScoped
-public class categoryManagementManagedBean implements Serializable
+@ViewScoped
+public class CategoryManagementManagedBean implements Serializable
 {
 
     @EJB(name = "ProductControllerLocal")
@@ -52,7 +53,7 @@ public class categoryManagementManagedBean implements Serializable
     
     
     
-    public categoryManagementManagedBean() {
+    public CategoryManagementManagedBean() {
     }
     
     @PostConstruct

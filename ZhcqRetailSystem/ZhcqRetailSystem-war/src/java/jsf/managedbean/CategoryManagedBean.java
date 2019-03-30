2f3fce9bbd7c2af87e5851c52b342cbb1b125773
Lim Cheng Yang <e0203248@u.nus.edu>
2019-03-30 21:56:12 +0800

    finished category crud managedBean and view

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/jsf/managedbean/CategoryManagedBean.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/jsf/managedbean/CategoryManagedBean.java
@@ -10,8 +10,16 @@ import entity.Category;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import util.exception.CategoryNotFoundException;
+import util.exception.CreateNewCategoryException;
+import util.exception.DeleteCategoryException;
+import util.exception.InputDataValidationException;
+import util.exception.UpdateCategoryException;
 
 /**
  *
@@ -23,18 +31,56 @@ public class CategoryManagedBean {
 
     @EJB(name = "CategoryControllerLocal")
     private CategoryControllerLocal categoryControllerLocal;
-    
 
     private List<Category> categories;
     private List<Category> filteredCategories;
-    
-    private Category selectedCategory;
-    
+
+    private Category newCategory;
+    private Category categoryToView;
+    private Category categoryToUpdate;
+    private Category categoryToDelete;
+
     public CategoryManagedBean() {
     }
-    
+
     @PostConstruct
     public void postConstruct() {
+        categories = categoryControllerLocal.retrieveAllCategories();
+        newCategory = new Category();
+    }
+
+    public void createNewCategory(ActionEvent event) {
+        try {
+            categoryControllerLocal.createNewCategoryEntity(newCategory);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Category created successfully", null));
+            newCategory = new Category();
+        } catch (CreateNewCategoryException | InputDataValidationException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to create category" + ex.getMessage(), null));
+        }
+    }
+
+    public void deleteCategory(ActionEvent event) {
+        try {
+            categoryToDelete = (Category) event.getComponent().getAttributes().get("categoryToDelete");
+            categoryControllerLocal.deleteCategory(categoryToDelete.getCategoryId());
+            categories.remove(categoryToDelete);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successfully deleted category", null));    
+        } catch (CategoryNotFoundException | DeleteCategoryException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to delete category " + ex.getMessage(), null));
+        }
+    }
+    
+    public void doUpdateCategory(ActionEvent event) {
+        categoryToUpdate = (Category) event.getComponent().getAttributes().get("categoryToUpdate");
+    }
+
+    public void updateCategory(ActionEvent event) {
+        try {
+            categoryControllerLocal.updateCategory(categoryToUpdate);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Successfully updated category", null));
+        } catch (CategoryNotFoundException | InputDataValidationException | UpdateCategoryException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to update category" + ex.getMessage(), null));
+        }
     }
 
     public List<Category> getCategories() {
@@ -53,14 +99,35 @@ public class CategoryManagedBean {
         this.filteredCategories = filteredCategories;
     }
 
-    public Category getSelectedCategory() {
-        return selectedCategory;
+    public Category getCategoryToUpdate() {
+        return categoryToUpdate;
     }
 
-    public void setSelectedCategory(Category selectedCategory) {
-        this.selectedCategory = selectedCategory;
+    public void setCategoryToUpdate(Category categoryToUpdate) {
+        this.categoryToUpdate = categoryToUpdate;
     }
-    
-    
-    
+
+    public Category getCategoryToDelete() {
+        return categoryToDelete;
+    }
+
+    public void setCategoryToDelete(Category categoryToDelete) {
+        this.categoryToDelete = categoryToDelete;
+    }
+
+    public Category getCategoryToView() {
+        return categoryToView;
+    }
+
+    public void setCategoryToView(Category categoryToView) {
+        this.categoryToView = categoryToView;
+    }
+
+    public Category getNewCategory() {
+        return newCategory;
+    }
+
+    public void setNewCategory(Category newCategory) {
+        this.newCategory = newCategory;
+    }
 }

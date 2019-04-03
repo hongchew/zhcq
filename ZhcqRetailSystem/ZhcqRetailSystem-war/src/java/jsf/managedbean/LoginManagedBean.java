4f139c89abdd8d63cc8063f4ec745e7c02411f43
ZhiMing96 <e0200805@u.nus.edu>
2019-04-04 02:49:45 +0800

    Fixed Login and Main Menu Bugs.

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/jsf/managedbean/LoginManagedBean.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/jsf/managedbean/LoginManagedBean.java
@@ -5,12 +5,11 @@ import ejb.stateless.StaffControllerLocal;
 import entity.Staff;
 import java.io.IOException;
 import java.io.Serializable;
-import javafx.event.ActionEvent;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 import util.exception.InvalidLoginCredentialException;
@@ -19,7 +18,7 @@ import util.exception.InvalidLoginCredentialException;
 @Named(value = "loginManagedBean")
 @RequestScoped
 
-public class LoginManagedBean{
+public class LoginManagedBean implements Serializable {
 
     @EJB(name = "StaffControllerLocal")
     private StaffControllerLocal staffControllerLocal;
@@ -30,17 +29,15 @@ public class LoginManagedBean{
     public LoginManagedBean() {
     }
 
-    @PostConstruct
-    public void postConstruct() {
-    }
-
     public void login(ActionEvent event) throws IOException {
+        System.out.println("ENTERED LOGIN METHOD IN MANAGED BEAN");
         try {
             Staff currentStaff = staffControllerLocal.staffLogin(username, password);
             FacesContext.getCurrentInstance().getExternalContext().getSession(true);
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("currentStaff", currentStaff);
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isLogin", true);
             FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+        
         } catch (InvalidLoginCredentialException ex) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid login credential: " + ex.getMessage(), null));
         }

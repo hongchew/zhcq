5b40e5f08fa9b0b888a18e58168ed40bb23bcecd
ZhiMing96 <e0200805@u.nus.edu>
2019-04-06 04:10:14 +0800

    completed retrieveAllProducts rws method. passes through the correct json

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/ProductResource.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/ProductResource.java
@@ -1,16 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ws.restful;
 
-import ejb.stateless.CategoryController;
-import ejb.stateless.CategoryControllerLocal;
 import ejb.stateless.ProductControllerLocal;
 import entity.Category;
+import entity.CoordinatedOutfit;
 import entity.ProductEntity;
+import entity.ProductTag;
+import entity.Promotion;
+import entity.ShoppingCart;
+import entity.WishList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,6 +20,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import util.exception.CategoryNotFoundException;
 import util.exception.ProductNotFoundException;
@@ -28,15 +31,17 @@ import ws.datamodel.RetrieveProductByIdRsp;
 
 
 @Path("Product")
+
 public class ProductResource {
     @Context
     private UriInfo context;
     
     private ProductControllerLocal productControllerLocal;
-    private CategoryControllerLocal categoryControllerLocal;
+    
 
-    public ProductResource(ProductControllerLocal productControllerLocal) {
-        this.productControllerLocal = productControllerLocal;
+    public ProductResource()
+    {
+        productControllerLocal = lookupProductControllerLocal();
     }
     
     @Path("retrieveAllProducts")
@@ -47,10 +52,49 @@ public class ProductResource {
         try
         { 
             List<ProductEntity> allProducts = productControllerLocal.retrieveAllProducts();
-        
+            //System.out.println("REACHED 1 Length of Array = " + allProducts.size());
+            
+            if(allProducts != null){
+                for(ProductEntity product:allProducts)
+                {
+                   
+                    Category category = product.getProductCategory();
+                    category.getProductEntities().clear();
+                    
+                    for(ProductTag tag: product.getProductTags())
+                    {
+                        tag.getProductEntities().clear();
+                    }           
+                   
+                    for(WishList wishlist: product.getWishLists())
+                    {
+                        wishlist.getProductEntities().clear();
+                    }     
+                    
+                    for(ShoppingCart cart: product.getShoppingcarts())
+                    {
+                        cart.getProducts().clear();
+                    }
+                    
+                    
+                    CoordinatedOutfit outfit = product.getCoordinatedOutfit();
+                    if(outfit !=null){
+                        outfit.getProductEntities().clear();
+                    }
+                 
+                    
+                    Promotion promotion = product.getPromotion();
+                    
+                    if(promotion !=null){
+                        promotion.getPromotionalProducts().clear();  
+                    }
+                }
+            }
+             
+           
             RetrieveAllProductsRsp retrieveAllProductsRsp  = new RetrieveAllProductsRsp(allProducts);
         
-            return Response.status(Response.Status.OK).entity(retrieveAllProductsRsp).build();
+            return Response.status(Status.OK).entity(retrieveAllProductsRsp).build();
         }
         catch(Exception ex)
         {
@@ -99,6 +143,22 @@ public class ProductResource {
         }
     }
     
+    
+    private ProductControllerLocal lookupProductControllerLocal()
+    {
+        
+        System.out.println("ENTERED INTO LOOK UP SESSION BEAN METHOD");
+        try
+        {
+            javax.naming.Context c = new InitialContext();
+            return (ProductControllerLocal) c.lookup("java:global/ZhcqRetailSystem/ZhcqRetailSystem-ejb/ProductController!ejb.stateless.ProductControllerLocal");
+        } 
+        catch (NamingException ne) 
+        {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
     
     
     

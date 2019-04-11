0fcea253ec11fd87b9b2091427ad7bac2b844e28
Lim Cheng Yang <e0203248@u.nus.edu>
2019-04-11 14:00:34 +0800

    memberResouce

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/MemberResource.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/MemberResource.java
@@ -13,31 +13,33 @@ import java.util.logging.Logger;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
+import util.exception.DeleteMemberException;
 import util.exception.InputDataValidationException;
+import util.exception.MemberNotFoundException;
 import ws.datamodel.CreateMemberReq;
 import ws.datamodel.CreateMemberRsp;
 import ws.datamodel.ErrorRsp;
 import ws.datamodel.RetrieveAllMembersRsp;
+import ws.datamodel.RetrieveMemberRsp;
 
 @Path("Member")
-public class MemberResource 
-{
+public class MemberResource {
 
     private MemberControllerLocal memberControllerLocal;
-    
+
     @Context
     private UriInfo context;
-    
-    
 
     public MemberResource() {
         memberControllerLocal = lookupMemberControllerLocal();
@@ -52,26 +54,26 @@ public class MemberResource
             throw new RuntimeException(ne);
         }
     }
-    
+
     @Path("retrieveAllMembers")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response retriveAllMembers() {
-        try{
+        try {
             List<Member> member = memberControllerLocal.retrieveAllMembers();
             RetrieveAllMembersRsp retrieveAllMembersRsp = new RetrieveAllMembersRsp(member);
             return Response.status(Status.OK).entity(retrieveAllMembersRsp).build();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
         }
     }
-    
+
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createMember(CreateMemberReq createMemberReq) {
-        if(createMemberReq != null) {
+        if (createMemberReq != null) {
             try {
                 Member member = createMemberReq.getMember();
                 Member newMember = memberControllerLocal.createNewMember(member);
@@ -80,7 +82,7 @@ public class MemberResource
             } catch (InputDataValidationException ex) {
                 ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
                 return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
-            } catch(Exception ex) {
+            } catch (Exception ex) {
                 ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
             }
@@ -89,7 +91,31 @@ public class MemberResource
             return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
         }
     }
+
+    @Path("retrieveMember")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response retrieveMember(@PathParam("id") Long id) {
+        try {
+            Member member = memberControllerLocal.retrieveMemberById(id);
+            RetrieveMemberRsp retrieveMemberRsp = new RetrieveMemberRsp(member);
+            return Response.status(Response.Status.OK).entity(retrieveMemberRsp).build();
+        } catch (MemberNotFoundException ex) {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
+        }
+    }
     
-    
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteMember(@PathParam("id") Long id) {
+        try {
+            memberControllerLocal.deleteMember(id);
+            return Response.status(Response.Status.OK).build();
+        } catch (DeleteMemberException | MemberNotFoundException ex) {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
+        }
+    }
+
 }
- 

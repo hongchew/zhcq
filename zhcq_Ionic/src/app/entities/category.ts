97ef1f2aaf88521bd0b7b72edca3e385ea2a11e7
ZhiMing96 <e0200805@u.nus.edu>
2019-04-11 14:34:31 +0800

    product entity ts file

--- zhcq_Ionic/src/app/entities/category.ts
+++ zhcq_Ionic/src/app/entities/category.ts
@@ -1,8 +1,20 @@
+import { ProductEntity } from './product';
+import { WishList } from './wishlist';
+import { CoordinatedOutfit } from './outfit';
+import { ShoppingCart } from './cart';
+import { Promotion } from './promotion';
+
 export class Category
 {
     categoryId: number; 
     categoryName: string;
     description: string; 
-
-    productEntities: productEntity[];
+    picturePath: string; 
+    
+    productEntities: ProductEntity[];
+    wishLists: WishList[];
+    coordinateOutfits: CoordinatedOutfit[];
+    shoppingCarts: ShoppingCart[];
+    promotion: Promotion[];
+    
 }

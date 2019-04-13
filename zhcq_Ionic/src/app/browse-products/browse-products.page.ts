f15994c9136658842d34d7f3c870620c1676491b
Qianyi Lim <limqianyi@gmail.com>
2019-04-13 18:23:44 +0800

    login and register

--- zhcq_Ionic/src/app/browse-products/browse-products.page.ts
+++ zhcq_Ionic/src/app/browse-products/browse-products.page.ts
@@ -35,8 +35,6 @@ export class BrowseProductsPage implements OnInit {
           //console.log(response);
           this.products = response.products 
           
-          
-          // imagePath = "../ZhcqRetailSystem/ZhcqRetailSystem-war/build/web/systemAdministration/images/canyon_top_black.jpg";
         },
         error => {
           this.errorMessage = error 
@@ -56,30 +54,5 @@ export class BrowseProductsPage implements OnInit {
       );
     }
   }
-
-  // ionViewLoaded() {
-
-  //   let rowNum = 0; //counter to iterate over the rows in the grid
-  
-  //   for (let i = 0; i < this.images.length; i+=2) { //iterate images
-  
-  //     this.grid[rowNum] = Array(2); //declare two elements per row
-  
-  //     if (this.images[i]) { //check file URI exists
-  //       this.grid[rowNum][0] = this.images[i] //insert image
-  //     }
-  
-  //     if (this.images[i+1]) { //repeat for the second image
-  //       this.grid[rowNum][1] = this.images[i+1]
-  //     }
-  
-  //     rowNum++; //go on to the next row
-  //   }
-  
-  // }
-
-  // openProductPage(product){
-  //   this.navCtrl.push('ProductDetails', {"product": product} );
-  // }
 
 }

b27af6afb857f43219a5ecd73f895926197d472b
Toh Hong Chew <hongchew@u.nus.edu>
2019-04-13 20:32:02 +0800

    added promotion restful

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/ApplicationConfig.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/ApplicationConfig.java
@@ -23,6 +23,7 @@ public class ApplicationConfig extends Application
         resources.add(ws.restful.CategoryResource.class);
         resources.add(ws.restful.MemberResource.class);
         resources.add(ws.restful.ProductResource.class);
+        resources.add(ws.restful.PromotionResource.class);
         resources.add(ws.restful.ShoppingCartResource.class);
         resources.add(ws.restful.WishListResource.class);
     }

72144940a254e14669a8f139da67a9b3b728ed00
ZhiMing96 <e0200805@u.nus.edu>
2019-04-15 01:50:30 +0800

    fixed promotional webservice bug

--- ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/PromotionResource.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-war/src/java/ws/restful/PromotionResource.java
@@ -6,22 +6,29 @@
 package ws.restful;
 
 import ejb.stateless.PromotionControllerLocal;
+import entity.Category;
+import entity.CoordinatedOutfit;
 import entity.ProductEntity;
 import entity.ProductTag;
 import entity.Promotion;
+import entity.ShoppingCart;
+import entity.WishList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 import util.exception.PromotionNotFoundException;
+import ws.datamodel.ErrorRsp;
 import ws.datamodel.RetrieveAllPromotionRsp;
 import ws.datamodel.RetrievePromotionByIdRsp;
 
@@ -31,32 +38,69 @@ import ws.datamodel.RetrievePromotionByIdRsp;
  */
 @Path("Promotion")
 public class PromotionResource {
+    
+    @Context
+    private UriInfo context;
+    
+    private PromotionControllerLocal promotionController;
 
-    PromotionControllerLocal promotionController = lookupPromotionControllerLocal();
+    public PromotionResource() {
+        promotionController = lookupPromotionControllerLocal();
+    }
+    
+    
 
     @Path("retrieveAllPromotions")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response retrieveAllPromotions(){
+        System.out.println("Entered Into Retrieve All Promotions Controller");
         
-        List<Promotion> promotions = promotionController.retrieveAllPromotions();
-        
-        for(Promotion promo : promotions){
-            for(ProductEntity pdt : promo.getPromotionalProducts()){
-                pdt.setShoppingcarts(null);
-                pdt.setWishLists(null);
-                pdt.setPromotion(null);
-                pdt.getProductCategory().setProductEntities(null);
-                for(ProductTag tag : pdt.getProductTags()){
-                    tag.setProductEntities(null);
+        try{
+            List<Promotion> promotions = promotionController.retrieveAllPromotions();
+            
+            System.out.println("got list of promotions");
+            
+            for(Promotion promo : promotions){
+                System.out.println("Promo: " + promo.getPromotionName());
+                
+                List<ProductEntity> products = promo.getPromotionalProducts();
+                
+                if(products != null && !products.isEmpty())
+                {
+                    for(ProductEntity product : products){
+                        Category category = product.getProductCategory();
+                        category.getProductEntities().clear();
+            
+                        product.getProductTags().clear();
+
+                        product.getWishLists().clear();
+
+                        product.getShoppingcarts().clear();
+
+                        product.setCoordinatedOutfit(null);
+                        
+                        product.setPromotion(null);
+
+                    }
                 }
+                
             }
+        
+        
+            RetrieveAllPromotionRsp rsp = new RetrieveAllPromotionRsp(promotions);
+            
+            System.out.println("Retrieve All Promotion Rsp created !");
+            return Response.status(Status.OK).entity(rsp).build();
+            
+        } catch (Exception ex){
+            
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
         }
         
         
-        RetrieveAllPromotionRsp rsp = new RetrieveAllPromotionRsp(promotions);
-        
-        return Response.status(Response.Status.OK).entity(rsp).build();
         
     }
     
@@ -88,10 +132,13 @@ public class PromotionResource {
     }
     
     private PromotionControllerLocal lookupPromotionControllerLocal() {
-        try {
-            Context c = new InitialContext();
+        try 
+        {
+            javax.naming.Context c = new InitialContext();
             return (PromotionControllerLocal) c.lookup("java:global/ZhcqRetailSystem/ZhcqRetailSystem-ejb/PromotionController!ejb.stateless.PromotionControllerLocal");
-        } catch (NamingException ne) {
+        }
+        catch (NamingException ne) 
+        {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
             throw new RuntimeException(ne);
         }

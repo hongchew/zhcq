30f0807c49d0d8d33bed607efc7de90dbda7d4c2
ZhiMing96 <e0200805@u.nus.edu>
2019-04-14 16:30:50 +0800

    created new footer component and added a cart with link to view my shopping cart

--- zhcq_Ionic/src/app/components/shared-components.module.ts
+++ zhcq_Ionic/src/app/components/shared-components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent,FooterComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent,FooterComponent]
 })
 export class SharedComponentsModule { }

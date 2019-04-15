53f6983240a1298d6f939cc990ba9c295f16ac51
ZhiMing96 <e0200805@u.nus.edu>
2019-04-15 19:30:16 +0800

    Added New attributes for outftit

--- ZhcqRetailSystem/ZhcqRetailSystem-ejb/src/java/entity/CoordinatedOutfit.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-ejb/src/java/entity/CoordinatedOutfit.java
@@ -36,8 +36,15 @@ public class CoordinatedOutfit implements Serializable {
     
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false)
-   
     private Date dateCreated;
+    
+    @Column(nullable = true)
+    private String picturePath; 
+    
+    @Column(length = 128)
+    @Size(max = 128)
+    private String description;
+    
     
     @OneToMany(mappedBy = "coordinatedOutfit")
     private List<ProductEntity> productEntities;
@@ -47,11 +54,15 @@ public class CoordinatedOutfit implements Serializable {
         productEntities = new ArrayList<ProductEntity>();
     }
 
-    public CoordinatedOutfit(String outfitName) {
+
+    public CoordinatedOutfit(String outfitName, String description, String picturePath) {
         this();
-        
         this.outfitName = outfitName;
+        this.picturePath = picturePath;
+        this.description = description;
     }
+    
+    
     
     public void addProduct(ProductEntity product){
         if(product != null)

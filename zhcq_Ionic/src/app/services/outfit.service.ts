cc29b1e30dd9cce411a391c29bff2901ada93af8
ZhiMing96 <e0200805@u.nus.edu>
2019-04-15 16:23:54 +0800

    Outfit services

---
+++ zhcq_Ionic/src/app/services/outfit.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class OutfitService {
+    baseUrl: string = 'http://localhost:8000/ZhcqRetailSystem-war/Resources/Coordinatedoutfit'
+
+    constructor(private httpClient: HttpClient) {
+
+    }
+
+    retrieveAllOutfits(): Observable<any> {
+        return this.httpClient.get<any>(this.baseUrl + '/retrieveAllOutfits').pipe
+        (
+        catchError(this.handleError)
+        );
+    }
+
+    retrieveOutfit(): Observable<any>  {
+        return this.httpClient.get<any>(this.baseUrl + '/retrieveOutfitById').pipe
+        (
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        let errorMessage: string = '';
+
+        if (error.error instanceof ErrorEvent)
+        {
+            errorMessage = 'An unknown error has occurred: ' + error.error.message;
+        } else {
+            errorMessage = 'A HTTP error has occurred: ' + `HTTP ${error.status}: ${error.error.message}`;
+        }
+        console.error(errorMessage);
+
+        return throwError(errorMessage);
+    }
+}

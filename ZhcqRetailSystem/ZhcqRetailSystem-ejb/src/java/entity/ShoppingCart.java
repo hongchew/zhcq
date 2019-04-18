3bf55f5968e46f71eeee83362b58d3b6629f0131
ZhiMing96 <e0200805@u.nus.edu>
2019-04-19 00:47:10 +0800

    change in database constraint

--- ZhcqRetailSystem/ZhcqRetailSystem-ejb/src/java/entity/ShoppingCart.java
+++ ZhcqRetailSystem/ZhcqRetailSystem-ejb/src/java/entity/ShoppingCart.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 
 @Entity
@@ -25,7 +26,7 @@ public class ShoppingCart implements Serializable {
     @GeneratedValue(strategy = GenerationType.TABLE)
     private Long cartId;
         
-//    @NotNull
+    @NotNull
     @OneToOne(mappedBy ="shoppingCart")
     private Member member; 
     

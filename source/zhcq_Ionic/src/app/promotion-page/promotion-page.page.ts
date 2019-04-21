9ee9db6da4f9a58ecd1a2f7dc1d433a451c16a99
ZhiMing96 <e0200805@u.nus.edu>
2019-04-21 22:29:00 +0800

    commit

--- source/zhcq_Ionic/src/app/promotion-page/promotion-page.page.ts
+++ source/zhcq_Ionic/src/app/promotion-page/promotion-page.page.ts
@@ -3,6 +3,10 @@ import { PromotionService } from '../services/promotion.service';
 import { Promotion } from '../entities/promotion';
 import { AlertController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { ProductEntity } from '../entities/product';
+import { THIS_EXPR, IfStmt } from '@angular/compiler/src/output/output_ast';
+import { debounceTime } from 'rxjs/operators';
 
 
 
@@ -13,19 +17,39 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PromotionPagePage implements OnInit {
 
-  errorMessage = '';
-  promotion= new Promotion();
-  promotionId: number;;
+  public searchControl: FormControl;
+  products: Array<ProductEntity> = [];
+  retrievedProducts: Array<ProductEntity> = [];
 
-  constructor(private promotionService: PromotionService, private activatedRoute: ActivatedRoute, private alertController: AlertController) { }
+  errorMessage = '';
+  promotion = new Promotion();
+  promotionId: number;
+  endDate = '';
+
+
+// tslint:disable: max-line-length
+  constructor(private promotionService: PromotionService, private activatedRoute: ActivatedRoute, private alertController: AlertController) {
+    this.searchControl = new FormControl();
+   }
 
   ngOnInit() {
-    this.promotionId = parseInt(this.activatedRoute.snapshot.paramMap.get('promotionId'));
+    this.promotionId = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
     console.log("promo ID: " + this.promotionId);
-    this.promotionService.retrievePromotionByPromotionId(this.promotionId).subscribe(
+    this.promotionService.retrievePromotionById(this.promotionId).subscribe(
       response => {
         this.promotion = response.promotion;
-        console.log('promo retrieved = ' + this.promotion.promotionName);
+        if (this.promotion != null) {
+          this.endDate = this.promotion.endDate.toString();
+          console.log('promo retrieved = ' + this.promotion.promotionName);
+          this.retrievedProducts = this.promotion.promotionalProducts;
+          this.products = this.retrievedProducts;
+          // search bar
+          this.searchControl.valueChanges.pipe(debounceTime(700)).subscribe(search => {
+            this.products = this.retrievedProducts;
+            this.setFilteredItems(search);
+        });
+        }
+
       },
       error => {
         this.errorMessage = error;
@@ -33,6 +57,23 @@ export class PromotionPagePage implements OnInit {
       }
     );
    }
+// search bar
+   setFilteredItems(searchTerm: string) {
+    if (searchTerm != null) {
+      this.products = this.filterItems(searchTerm);
+    }
+  }
+// search bar
+  filterItems(searchTerm: string) {
+    console.log('PRODUCTS ARRAY IN FILTER ITEMS METHOD: ' + this.products);
+
+    return this.products.filter(product => {
+      console.log('Item Name = ' + product.productName);
+      console.log('Search Term = ' + searchTerm);
+
+      return product.productName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
+  }
 
    async presentAlert(message: string) {
     const alert = await this.alertController.create({
